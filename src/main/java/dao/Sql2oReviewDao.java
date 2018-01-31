a9104cdf75bbb72bb2cd168b1235a3ecc8e249bc
Global User <staff@epicodus.com>
2018-01-31 13:31:31 -0800

    change returngenerated keys to true in add methods

--- src/main/java/dao/Sql2oReviewDao.java
+++ src/main/java/dao/Sql2oReviewDao.java
@@ -20,7 +20,7 @@ public class Sql2oReviewDao implements ReviewDao {
     public void add(Review review) {
         String sql = "INSERT INTO reviews (writtenby, rating, content, restaurantid, createdat) VALUES (:writtenBy, :rating, :content, :restaurantId, :createdat)";
         try (Connection con = sql2o.open()) {
-            int id = (int) con.createQuery(sql)
+            int id = (int) con.createQuery(sql, true)
                     .bind(review)
                     .executeUpdate()
                     .getKey();
@@ -59,22 +59,4 @@ public class Sql2oReviewDao implements ReviewDao {
         }
     }
 
-    @Override
-    public List<Review> getAllReviewsByRestaurantSortedNewestToOldest(int restaurantId) {
-        List<Review> unsortedReviews = getAllReviewsByRestaurant(restaurantId);
-        List<Review> sortedReviews = new ArrayList<>();
-
-        for (int i = 0; i < unsortedReviews.size()-1; i++) {
-            int comparisonResult = unsortedReviews.get(i).compareTo(unsortedReviews.get(i + 1));
-            if (comparisonResult == -1) { //first object was made earlier than second object
-                sortedReviews.add(0,unsortedReviews.get(i + 1)); //push the second object to the top of the list.
-            } else if (comparisonResult == 0) {
-                sortedReviews.add(0,unsortedReviews.get(i)); //probably should have a tie breaker here as they are the same.
-            } else {
-                sortedReviews.add(0,unsortedReviews.get(i)); //push the first object to the list as it is newer than the second object.
-            }
-        }
-
-    return sortedReviews;
-    }
 }
